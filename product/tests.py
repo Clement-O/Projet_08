0df7616e8cb54ef92332a65513b9c20223f9855b
Clement-O <omont.clement@gmail.com>
2019-05-10 19:39:58 +0200

    Tests & preparation deployement on Heroku

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+from .input import UserInput
+from .substitute import SubstituteQueries
+from .refine import RefineSubstitute
+
 
 # Create your tests here.
+
+
+def test_get_product(monkeypatch):
+    query = "nutella"
+    result = {
+        'cat_1': 'value_1',
+        'cat_2': 'value_2',
+        'cat_3': 'value_3',
+        'ng': 'ng',
+    }
+
+    def mockreturn(query):
+        return result
+
+    path = 'product.input.UserInput.get_product'
+    monkeypatch.setattr(path, mockreturn)
+    assert UserInput.get_product(query) == result
+
+
+def test_query_off(monkeypatch):
+    product = {
+        'cat_1': 'value_1',
+        'cat_2': 'value_2',
+        'cat_3': 'value_3',
+        'ng': 'ng',
+    }
+    result = {
+        'id': 123456,
+        'name': 'sub',
+        'img': 'link_off'
+    }
+
+    def mockreturn(product):
+        return result
+
+    path = 'product.substitute.SubstituteQueries.query_off'
+    monkeypatch.setattr(path, mockreturn)
+    assert SubstituteQueries.query_off(product) == result
+
+
+def test_product_infos(monkeypatch):
+    products = [
+        {'product_name': 'p_D', 'nutrition_grades': 'd'},
+        {'product_name': 'p_B', 'nutrition_grades': 'b'},
+        {'product_name': 'p_A', 'nutrition_grades': 'a'}
+    ]
+    result = [
+        {'product_name': 'p_A', 'nutrition_grades': 'a'},
+        {'product_name': 'p_B', 'nutrition_grades': 'b'},
+        {'product_name': 'p_D', 'nutrition_grades': 'd'}
+    ]
+
+    def mockreturn(products):
+        return result
+
+    path = 'product.refine.RefineSubstitute.product_infos'
+    monkeypatch.setattr(path, mockreturn)
+    assert RefineSubstitute.product_infos(products) == result
--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Third party import
+import dj_database_url
 from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -21,18 +22,13 @@ load_dotenv(dotenv_path)
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'purbeurre-coa.herokuapp.com']
 
 # Application definition
 
@@ -56,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'purbeurre.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -99,25 +95,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
-
 
 # Allauth
 # https://django-allauth.readthedocs.io/en/latest/installation.html
@@ -152,7 +150,6 @@ ACCOUNT_FORMS = {'signup': 'user.forms.CustomSignupForm'}
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -166,8 +163,31 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+if os.environ.get('ENV') == 'PRODUCTION':
+    DEBUG = False
+
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    STATICFILES_STORAGE = \
+        'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
+else:
+    DEBUG = True
