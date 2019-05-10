4f3d042ecced74c7c13fa102d11b5f60ab4a7c4d
Clement-O <omont.clement@gmail.com>
2019-05-10 18:55:24 +0200

    FrontEnd product

--- product/urls.py
+++ product/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'product'
 urlpatterns = [
-    path('search/', views.search, name='search'),
+    path('', views.search, name='search'),
     path('save/', views.save, name='save'),
     path('user/', views.user, name='user_product'),
     path('<int:product_id>/', views.detail, name='detail'),
