c962b0e7806502994ab492170562c19c67bb9efd
clement <o.clems@hotmail.fr>
2019-06-04 16:09:35 +0200

    load .env in wsgi

--- purbeurre/wsgi.py
+++ purbeurre/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Third party import
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre.settings')
 
 application = get_wsgi_application()
