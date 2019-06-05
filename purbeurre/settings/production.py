3b809d9d439381c01d390eee4e51b8a6b8668319
clement <o.clems@hotmail.fr>
2019-06-05 15:18:03 +0200

    correct prod error

--- purbeurre/settings/production.py
+++ purbeurre/settings/production.py
@@ -22,7 +22,7 @@ RAVEN_CONFIG = {
     # caution replace by your own!!
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
 }
 
 LOGGING = {
